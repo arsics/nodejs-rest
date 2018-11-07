f97d795655f3cfa2abccf1868b3fcb3b71f51e25
Sasha Arsic <ext.sasa.arsic@tieto.com>
2018-11-07 12:42:59 +0100

    NJSREST-006 DELETE request + refactor

--- njsrest04_1-rest/index.js
+++ njsrest04_1-rest/index.js
@@ -27,19 +27,13 @@ app.get('/api/courses/:year/:month', (req, res) => {
 
 app.get('/api/courses/:id', (req, res) => {
     const course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) {  // if we don't find the couse return 404
-        res.status('404').send('Course with that ID was not found');
-        return;
-    }
+    if (!course) return res.status('404').send('Course with that ID was not found');
     res.send(course);
 });
 
 app.post('/api/courses', (req, res) => {
     const { error } = validateCourse(req.body);
-    if (error) {
-        res.status(400).send(error.details[0].message);
-        return;
-    }
+    if (error) return res.status(400).send(error.details[0].message);
 
     const course = {
         id: courses.length + 1,
@@ -52,18 +46,20 @@ app.post('/api/courses', (req, res) => {
 app.put('/api/courses/:id', (req, res) => {
     // look up the course and validate, return 404 if not existing or 400 if invalid
     const course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) {
-        res.status('404').send('Course with that ID was not found');
-        return;
-    }
+    if (!course) return res.status('404').send('Course with that ID was not found');
     const { error } = validateCourse(req.body); // destructured body object
-    if (error) { // bad
-        res.status(400).send(error.details[0].message);
-        return;
-    }
+    if (error) return res.status(400).send(error.details[0].message);
     
     // update the course and return it
     course.name = req.body.name;
+    res.send(course);
+});
+
+app.delete('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) return res.status('404').send('Course with that ID was not found');
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);
     res.send(course);
 });
 
